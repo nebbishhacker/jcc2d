e8a9b9ce7dff2780503371a8de659982b99120b8
Jonathan <jonathankaralasingham@hotmail.com>
2013-03-19 08:20:43 -0400

    ye

--- JimmysCardboardCastle/JonathansTestWorld.h
+++ JimmysCardboardCastle/JonathansTestWorld.h
@@ -3,6 +3,11 @@
 #include "World.h"
 #include "entities.h"
 
+static void playTestSound2(World* world) {
+		Sound sound = soundEngine.loadSound("sound/Wind.mp3");
+		soundEngine.playSound(sound);
+		}
+
 class JonathansTestWorld : public World
 {
 public:
@@ -13,6 +18,10 @@ public:
 		//add(new CollTestBox(400, 200));
 
 		add(new Background(0, -100, 1600, 1200, "images/PGBackground.png", -100, 0.5, 0.25));
+
+		
+		add(new Button(300, -320, 68, 68, "images/FanBase.png", playTestSound2));
+
 
 		std::shared_ptr<TileSheet> tileSheet(new TileSheet(32, 32, 4, 1, "images/JCC_PlatSPR.png"));
 
@@ -35,3 +44,4 @@ public:
 		}
 	}
 };
+
