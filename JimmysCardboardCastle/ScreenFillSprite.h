f6b587c6dff139ce3c0649ae7c4da328a7139bf3
Ben Whittaker <benjaminwhittaker@uoit.net>
2013-03-23 13:52:24 -0400

    Apparently forgot to commit ScreenFillSprite.

---
+++ JimmysCardboardCastle/ScreenFillSprite.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Sprite.h"
+
+//  This sprite fills the screen with whatever color you set it to have upon creation. Can have alpha transparency.
+class ScreenFillSprite: public Sprite
+{
+public:
+	ScreenFillSprite(float red, float green, float blue, float alpha = 1.0);
+	virtual void draw(double cameraX, double cameraY);
+
+	float red;
+	float green;
+	float blue;
+	float alpha;
+};
