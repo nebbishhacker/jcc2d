e749534e131ec2857be66593bf241247c79a6d85
TheSECKS <brandonle_85@hotmail.com>
2013-04-08 16:27:03 -0400

    entities fixed

--- JimmysCardboardCastle/entities.h
+++ JimmysCardboardCastle/entities.h
@@ -8,10 +8,7 @@
 #include "BoxBoy.h"
 #include "Tracks.h"
 #include "DragonBoy.h"
-<<<<<<< HEAD
 #include "Cookie.h"
 #include "DBoyProjectile.h"
-=======
 #include "RobertsFriends.h"
 #include "Cookie.h"
->>>>>>> 86674856bfa932f28584e08924b57db3037abc1b
