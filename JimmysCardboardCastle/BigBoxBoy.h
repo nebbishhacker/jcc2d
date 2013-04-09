c8d3ff7dd547d08de094f2dfdd6fca9a1141d50d
TheSECKS <brandonle_85@hotmail.com>
2013-04-09 03:12:45 -0400

    added background elements to the first level. started working on big box boy.

---
+++ JimmysCardboardCastle/BigBoxBoy.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "physicssprite.h"
+class BigBoxBoy : public PhysicsSprite
+{
+public:
+	BigBoxBoy(double x, double y);
+	void initialize();
+	void update();
+
+	// state variables
+	bool walking;
+};
