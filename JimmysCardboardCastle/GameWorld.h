de1fc8903d13731e3b1cdfc4914f515879b44bed
TheSECKS <brandonle_85@hotmail.com>
2013-04-13 19:57:52 -0400

    Dat Milk Dunnnn.
    HP UI properly working.
    Max health is 5.

--- JimmysCardboardCastle/GameWorld.h
+++ JimmysCardboardCastle/GameWorld.h
@@ -10,6 +10,7 @@
 #include "levelConstructor.h"
 #include "QuitDialog.h"
 #include "LevelEditor.h"
+#include "healthHandler.h"
 
 class GameWorld : public World
 {
@@ -29,7 +30,7 @@ public:
 		Sprite * t;
 		
 		// Initialize HUD here //
-		add(new Background(0, 0, 800, 600, "images/PlainHud.png", 100, 0, 0));
+		//add(new Background(0, 0, 800, 600, "images/PlainHud.png", 100, 0, 0));
 		t = new Button(325, /*game->stateInfo.screenHeight - 36*/ 550, 139, 50, "images/button_menu.png", startWorld<QuitDialog>);
 		t->scrollFactorX = 0;
 		t->scrollFactorY = 0;
--- JimmysCardboardCastle/Player.cpp
+++ JimmysCardboardCastle/Player.cpp
@@ -1,5 +1,7 @@
 #include "Player.h"
 #include "Game.h"
+#include "GameWorld.h"
+#include "healthHandler.h"
 #include <iostream>
 
 Player::Player(double x, double y) : PhysicsSprite("images/aa_spr_sam.png")
@@ -58,12 +60,18 @@ void Player::initialize()
 	input = world->input;
 	world->groups["player"].add(this);
 	world->groups["ground"].add(this);
+
+	Sprite * t = new healthHandler(this, 5);
+	world->add(t);
 }
 
 void Player::update()
 {
 	if (input->keysPressed['z']) {
 		hasBoots = !hasBoots;
+	}
+	if (input->keysPressed['a']) {
+		health-=0.5;
 	}
 
 	if (collide(&world->groups["lava"], 0, -2) != NULL)
@@ -169,6 +177,8 @@ void Player::update()
 
 	// Figure out the net acceleration from forces //
 	netAcceleration += netForce / mass;
+
+	
 
 	// Move the sprite, checking for collisions //
 	Vector2D delta = velocity + netAcceleration * 0.5;
