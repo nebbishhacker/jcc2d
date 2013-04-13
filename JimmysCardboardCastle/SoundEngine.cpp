41535e9b9d8cc24ab428a0267c445a6b5f534adf
Ben Whittaker <benjaminwhittaker@uoit.net>
2013-04-13 17:58:38 -0400

    Messed with the sound engine a bit, made TextureCache a singleton.

--- JimmysCardboardCastle/SoundEngine.cpp
+++ JimmysCardboardCastle/SoundEngine.cpp
@@ -14,26 +14,33 @@ SoundEngine::~SoundEngine()
 
 Sound SoundEngine::loadSound(std::string fileName)
 {
+	FMOD::Sound * fmodSound;
 	Sound sound;
 	std::map<std::string, Sound>::iterator it = soundMap.find(fileName);
 	if (it == soundMap.end())
 	{
-		FMOD_RESULT result = system->createSound(fileName.c_str(), FMOD_2D, 0, &sound);
+		FMOD_RESULT result = system->createSound(fileName.c_str(), FMOD_2D, 0, &fmodSound);
 		if (result != FMOD_OK) std::cout << "Loading file \"" << fileName << "\" failed with code " << result << "\n";
+		sound = Sound(fmodSound);
 	}
 	else sound = soundMap[fileName];
 	return sound;
 }
-FMOD::Channel * SoundEngine::playSound(Sound sound)
+FMOD::Channel * SoundEngine::playSound(Sound &sound)
 {
 	FMOD::Channel * channel;
-	system->playSound(FMOD_CHANNEL_FREE, sound, false, &channel);
+	system->playSound(FMOD_CHANNEL_FREE, sound.fmodSound, false, &channel);
 	return channel;
 }
 
 void SoundEngine::update()
 {
 	system->update();
+}
+
+Sound::Sound(FMOD::Sound * fmodSound)
+{
+	this->fmodSound = fmodSound;
 }
 
 SoundEngine soundEngine;
